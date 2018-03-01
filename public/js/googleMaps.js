d70f406ed4de287f6607920087a4bdc5b06345c7
Mejan Ro <mrostami@ucsd.edu>
2018-03-01 10:50:26 -0800

    I was here

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat:32.880, lng:-117.236};
+
+	var map = new google.maps.Maps(document.getELementantById('maps'), {
+		center: ucsd.ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position; ucsd.ltlng,
+		maps: map,
+		title: "UCSD"
+	})
+
 }
